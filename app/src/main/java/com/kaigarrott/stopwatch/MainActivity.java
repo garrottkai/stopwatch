833a14bcf83a1c8318b2dbed6c04a00cff0039b4
garrottkai <garrottkai@gmail.com>
2019-06-10 17:46:02 +0800

    basic functionality working

--- app/src/main/java/com/kaigarrott/stopwatch/MainActivity.java
+++ app/src/main/java/com/kaigarrott/stopwatch/MainActivity.java
@@ -2,6 +2,8 @@ package com.kaigarrott.stopwatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.Date;
@@ -10,17 +12,19 @@ public class MainActivity extends AppCompatActivity {
 
     private StopwatchThread mThread;
     private TextView mOutputView;
+    private Boolean mRunning = false;
+    private final String TAG = this.getClass().getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mThread = new StopwatchThread();
         mOutputView = findViewById(R.id.output);
     }
 
     private void start() {
+        mThread = new StopwatchThread();
         mThread.start();
     }
 
@@ -28,21 +32,36 @@ public class MainActivity extends AppCompatActivity {
         mThread.interrupt();
     }
 
+    public void toggle(View v) {
+        if(mRunning) {
+            stop();
+        } else {
+            start();
+        }
+        mRunning = !mRunning;
+    }
+
     private class StopwatchThread extends Thread {
         @Override
         public void run() {
 
             super.run();
-            long now = new Date().getTime();
-            mOutputView.setText("00:00:00.00");
+            long begin = new Date().getTime();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mOutputView.setText("00:00:00.00");
+                }
+            });
 
             while(!interrupted()) {
                 try {
-                    long diff = new Date().getTime() - now;
-                    double h = Math.floor(diff / 3600000);
-                    double m = Math.floor((diff % 3600000) / 60000);
-                    double s = Math.floor((m % 60000) / 1000);
-                    double cs = Math.floor((s % 1000) / 10);
+                    long diff = new Date().getTime() - begin;
+                    int h = (int) Math.floor(diff / 3600000);
+                    int m = (int) Math.floor((diff % 3600000) / 60000);
+                    int s = (int) Math.floor((diff % 60000) / 1000);
+                    int cs = (int) Math.floor((diff % 1000) / 10);
 
                     String outH = "";
                     String outM = "";
